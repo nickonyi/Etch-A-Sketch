8004b468d4515194b808a32a074a74e3142843b7
nickonyi <nickoluoch1130@gmail.com>
2022-05-22 07:03:58 +0300

    Etch A Sketch

--- script.js
+++ script.js
@@ -1,47 +1,14 @@
 const container = document.querySelector('#container');
-const button = document.querySelector('.btn-grid-size');
 
-
-function addGrid(numberOfDivsToCreate) {
-
-    let gridSize = Math.pow(numberOfDivsToCreate, 2);
-
-    while (gridSize > 0) {
-        let newDiv = document.createElement('div');
+function createGrid(gridsize) {
+    for (i = 0; i < gridsize ** 2; i++) {
+        const newDiv = document.createElement('div');
         newDiv.classList.add('grid');
         newDiv.style.border = '1px solid black';
         container.appendChild(newDiv);
-
-        gridSize--;
+        container.style.gridTemplateColumns = `repea`;
     }
 
-
-    container.setAttribute(
-        'style',
-        `grid-template:repeat(${numberOfDivsToCreate},1fr)/repeat(${numberOfDivsToCreate},1fr);`
-    );
 }
 
-
-
-
-function changeColor() {
-    this.style.background = 'red';
-}
-
-function clear() {
-    let reqGridSize = prompt('How many squares do you want?');
-
-    if (reqGridSize >= 1 && reqGridSize <= 100) {
-        while (container.hasChildNodes()) {
-            container.removeChild(container.lastChild); //remove all the grid boxess
-        }
-        addGrid(reqGridSize);
-    } else {
-        alert("choose a number between 1 - 100");
-        clear();
-    }
-}
-
-button.addEventListener('click', clear);
-window.onload = addGrid(16);
+window.onload = createGrid(16);
